2e4f1fd7ce365498842f3b62299270ab3baa1303
Nick Angeli <nicangeli@gmail.com>
2021-09-29 14:49:04 +0100

    add knexfile and init migration

---
+++ migrations/20210929130657_init.js
@@ -0,0 +1,18 @@
+exports.up = function (knex) {
+    knex.schema.createTable('shop', (table) => {
+        table.increments('id').primary()
+
+        table.string('domain')
+
+        table.string('question')
+        table.string('answerLabel1')
+        table.string('answerLabel2')
+        table.string('answerLabel3')
+
+        table.string('buttonColor')
+
+        table.boolean('enabled')
+    })
+}
+
+exports.down = function (knex) {}
